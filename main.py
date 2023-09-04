108454337e1a078599eca856ca05b88c68e144d9
abdelkhalek hanbel <116838826+bhwayjoo@users.noreply.github.com>
2023-09-04 17:56:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,76 +1,78 @@
 import datetime
-user={}
-births=[]
+
+user = {}
+births = []
+
+
+def MaxAge():
+    for x in user.values():
+        return x
+
+
+def new():
+    now = datetime.datetime.today()
+    yearNow = int(now.year)
+    monthNow = int(now.month)
+    dayNow = int(now.day)
+
+    def year():
+        y = int(input('Please type your birth year: '))
+        if y > yearNow or y < 1800:
+            print('Invalid date')
+            return year()
+        return y
+
+    def month():
+        m = int(input('Please type your birth month: '))
+        if m < 1 or m > 12:
+            print('Invalid date')
+            return month()
+        return m
+
+    def day():
+        d = int(input('Please type your birth day: '))
+        if d < 1 or d > 31:
+            print('Invalid date')
+            return day()
+        return d
+
+    def age():
+        age = yearNow - birth.year - ((monthNow, dayNow) < (birth.month, birth.day))
+        return age
+
+    name = input("Type your name: ")
+    year_birth = year()
+    month_birth = month()
+    day_birth = day()
+    birth = datetime.date(year_birth, month_birth, day_birth)
+    births.append(birth)
+    age_val = age()
+    user.update({age_val: name})
+
+
 while True:
+    choice = input(
+        '-------------------------------------------------------------\n1: if you want to add a new user\n2: show users\nType a number: ')
 
-
-    def MaxAge() :
-        for x in user.values():
-            return (x)
-    def new():
-        now=datetime.datetime.today()
-        yearNow= int(now.year)
-        mouthNow= int (now.month)
-        dayNow=int (now.day)
-
-
-        def year():
-            y=int(input('Please tipe your birth year : '))
-            if y>yearNow or y<1800:
-                print('Invalid date')
-                new()
-            else:
-                return y
-        def mouth():
-            m=int(input('Please tipe your birth month : '))
-            if m<0 or m>12:
-                print('Invalid date')
-                new()
-            else:
-                return m
-        def day():
-            d=int(input('Please tipe your birth day : '))
-            if d<0 or d>31:
-                print('Invalid date')
-                new()
-            else:
-                return d
-
-        def age():
-            age = yearNow - birth.year - ((mouthNow, dayNow) < (birth.month, birth.day))
-            return age
-
-
-
-        name = input("tipe your name : ")
-        year = year()
-        mouth= mouth()
-        day= day()
-        birth=datetime.date(year,mouth,day)
-        births.append(birth)
-        age=age()
-        user.update({age:name})
-    a=input ('-------------------------------------------------------------\n1: if you add a new user \n2: show users   \nType a number :')
-    if a=="1":
+    if choice == "1":
         new()
-    elif a=="2":
-        b=0
-        for usr in user :
-            print (f'{user[usr]} is {usr} years old and she/he was born on {(births[b].strftime("%A") )} ')
+    elif choice == "2":
+        b = 0
+        for usr_age, usr_name in user.items():
+            birth_date = births[b].strftime("%A")
+            print(f'{usr_name} is {usr_age} years old and was born on {birth_date}')
+            b += 1
 
-            b=1+b
-        if (len(user))==1 or (len(user))==0 :
-            print ("There is no oldest or youngest person")
+        if len(user) == 1 or len(user) == 0:
+            print("There is no oldest or youngest person")
         else:
-            minAge = (min(user.keys()))
-            maxAge = max(user.keys())
-            print(f'The oldest one is {user[maxAge]}')
-            print(f'The youngest one is {user[minAge]}')
-            print (f'Total People: {(len(user))}')
-
-
+            min_age = min(user.keys())
+            max_age = max(user.keys())
+            print(f'The oldest one is {user[max_age]}')
+            print(f'The youngest one is {user[min_age]}')
+            print(f'Total People: {len(user)}')
     else:
-        print ("invali choit")
+        print("Invalid choice")
 
 
 
